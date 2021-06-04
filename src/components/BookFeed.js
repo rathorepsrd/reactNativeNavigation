c99619aa532a11845e34c41e59f947aa096a2538
Pooja <rathorepsrd@gmail.com>
2021-06-04 16:13:36 +0530

    BookFeed component flatlist craete for feed

---
+++ src/components/BookFeed.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import {FlatList, View, Text, StyleSheet } from 'react-native';
+// import styled from 'styled-components/native';
+
+const books = [
+  { id: 0, title: 'JavaScript & JQuery: Web Development' },
+  { id: 1, title: 'To Sleep in a Cloud of Stars' },
+  { id: 2, title: 'Dance on the Moon' },
+  { id: 3, title: 'Best birthday party with enemies' },
+  { id: 4, title: 'Second thought changed my life' },
+  { id: 5, title: 'Master data science with deep neural network' },
+  { id: 6, title: 'Rest Api with SpringBoot Data Rest Modified Title2' },
+  { id: 7, title: 'Rest Api with Django rest framework' },
+  { id: 8, title: 'Change life with Yoga' },
+  { id: 9, title: 'How the mind works' },
+  { id: 10, title: 'How to analyze people' }
+];
+
+// const ContentView = styled.View`
+//     height: 100;
+//     overflow: hidden;
+//     margin-left: 10px;
+// `;
+// const Separator = styled.View`
+    // height: 1;
+    // width: 100%;
+    // background-color: #ced0ce;
+// `;
+
+const BookFeed = () => {
+    return(
+        <View>
+              <FlatList
+                data={books}
+                keyExtractor={({ id }) => id.toString()}
+                ItemSeparatorComponent={() => <View style={styles.seperator} />}
+                renderItem={({ item }) => <Text style={[styles.itemTitle , styles.item]}>{item.title}</Text>
+              }
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: 'lightblue',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  itemTitle: {
+    fontSize: 32,
+  },
+  seperator: {
+    height: 1,
+    width: '100%',
+    backgroundColor: '#ced0ce',
+  }
+})
+export default BookFeed;
--- src/screens/feed.js
+++ src/screens/feed.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { Text, View,  Button} from 'react-native';
+import { Text, View,  Button, StyleSheet} from 'react-native';
+import BookFeed from '../components/BookFeed';
+
 const Feed = props => {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Feed</Text>
+        <View style={{ padding: 40, flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            
+            <Text>Book Feed Data</Text>
+            <BookFeed />
             <Button
                 title="Read Book"
                 onPress={() => props.navigation.navigate('Book')}
@@ -15,4 +19,5 @@ const Feed = props => {
 Feed.navigationOptions = {
     title: 'Feed'
 };
+
 export default Feed;
